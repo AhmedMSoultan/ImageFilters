1211a718ca6e61cf484d06502996b16f510f0418
Ahmed Soultan <ahmed.soultan@nweave.com>
2023-11-30 23:41:49 +0200

    Initial Commit

---
+++ ImageFilters/ImageFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFiltersApp.swift
+//  ImageFilters
+//
+//  Created by Ahmed Soultan on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
